6facacd4788dc5f274b820e1c77e192699380ffc
Pavless <xsevci63@vutbr.cz>
2020-02-08 13:04:17 +0100

    Config change

--- train_64x64-min1024.py
+++ train_64x64-min1024.py
@@ -34,7 +34,7 @@ if 1:
     desc += '-64x64-min1024';     dataset = EasyDict(tfrecord_dir='64x64-min1024')
 
     # Number of GPUs.
-    desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4
+    desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32}
 
     # Default options.
     train.total_kimg = 8000
